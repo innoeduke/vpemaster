712fb29d613385f19f5ae29b014526324c363074
PGCaptain <kylewei@gmail.com>
2025-11-23 23:14:54 +0800

    feat(contacts): #15 show KPIs of total contacts and total members

--- app/contacts_routes.py
+++ app/contacts_routes.py
@@ -31,8 +31,14 @@ def show_contacts():
         return redirect(url_for('agenda_bp.agenda'))
 
     contacts = Contact.query.outerjoin(Contact.user).order_by(Contact.Name.asc()).all()
+    
+    # 计算联系人总数和成员数（包括官员）
+    total_contacts = Contact.query.count()
+    total_members = Contact.query.filter(Contact.Type.in_(['Member', 'Officer'])).count()
+    
     pathways = list(current_app.config['PATHWAY_MAPPING'].keys())
-    return render_template('contacts.html', contacts=contacts, pathways=pathways)
+    return render_template('contacts.html', contacts=contacts, pathways=pathways, 
+                         total_contacts=total_contacts, total_members=total_members)
 
 
 @contacts_bp.route('/contact/form', methods=['GET', 'POST'])
