a8fc4a8b4a520fec19195bf999898aff69d91f20
PGCaptain <kylewei@gmail.com>
2025-12-29 20:27:34 +0800

    feat: Update club logos and fix `remove_manager_role` script's import path.

--- scripts/remove_manager_role.py
+++ scripts/remove_manager_role.py
@@ -1,3 +1,7 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from app import create_app, db
 from app.models import Role, User, LevelRole
