cac3bbcc753360b0b0607389c54b158c9ffcad37
Rich <rich@moleqode.com>
2026-05-14 15:30:55 +0800

    switch to ssl 465 port on mail server

--- config.py
+++ config.py
@@ -47,9 +47,9 @@ class Config:
     AVATAR_ROOT_DIR = 'uploads/avatars'
 
     MAIL_SERVER = os.getenv('MAIL_SERVER', 'smtp.gmail.com')
-    MAIL_PORT = int(os.getenv('MAIL_PORT', 587))
-    MAIL_USE_TLS = os.getenv('MAIL_USE_TLS', 'true').lower() in ['true', 'on', '1']
-    MAIL_USE_SSL = os.getenv('MAIL_USE_SSL', 'false').lower() in ['true', 'on', '1']
+    MAIL_PORT = int(os.getenv('MAIL_PORT', 465))
+    MAIL_USE_TLS = os.getenv('MAIL_USE_TLS', 'false').lower() in ['true', 'on', '1']
+    MAIL_USE_SSL = os.getenv('MAIL_USE_SSL', 'true').lower() in ['true', 'on', '1']
     MAIL_USERNAME = os.getenv('MAIL_USERNAME')
     MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
     MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER', 'support.memmaker@gmail.com')
