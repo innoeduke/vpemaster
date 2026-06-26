d3a4445d7bb573030e275637ce7172dd4923c6aa
PGCaptain <kylewei@gmail.com>
2026-06-26 20:42:43 +0800

    debug(voting): log before_request outcome

--- app/voting_routes.py
+++ app/voting_routes.py
@@ -59,6 +59,8 @@ def short_circuit_cached_voting():
     club_id = get_current_club_id()
     cache_key = f"voting_html_guest_{club_id}_{meeting_id or 'default'}"
     cached_html = cache.get(cache_key)
+    import sys
+    print(f"BR endpoint={request.endpoint} key={cache_key} hit={cached_html is not None}", file=sys.stderr, flush=True)
     if cached_html is None:
         return None
 
